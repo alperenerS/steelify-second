ba23839d2bbb72201839b932004bd535a9746aa2
emremataraci <mtrcemre@gmail.com>
2024-07-26 16:05:28 +0300

    Feature: Assets removed. Videos & Images shown with url

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView, Linking } from 'react-native';
-import { Card, Title, Paragraph, Button, Text, Image } from 'react-native-paper';
+import { Card, Title, Paragraph, Button, Text } from 'react-native-paper';
 import CustomVideoPlayer from '../components/CustomVideoPlayer';
 import homeScreenStyles from '../styles/HomeScreenStyles';
 
@@ -13,7 +13,7 @@ const HomeScreen = ({ navigation }) => {
     <ScrollView style={homeScreenStyles.container}>
       <Text style={homeScreenStyles.header}>Kaynak ve Büküm</Text>
       <Card style={homeScreenStyles.topSection}>
-        <Card.Cover source={{ uri: 'https://example.com/strong-core.jpg' }} />
+        <Card.Cover source={{ uri: 'https://yenastorage.blob.core.windows.net/steelify/steelify_sample_image1.jpg' }} />
         <Card.Content>
           <Title>DAHA GÜÇLÜ KAYNAK TEKNİKLERİ</Title>
         </Card.Content>
@@ -29,21 +29,21 @@ const HomeScreen = ({ navigation }) => {
       </Card>
       <ScrollView horizontal>
         <Card style={homeScreenStyles.card}>
-          <CustomVideoPlayer source={require('../assets/videos/sample_video1.mp4')} style={homeScreenStyles.video} />
+          <CustomVideoPlayer source={{ uri: 'https://yenastorage.blob.core.windows.net/steelify/sample_video1.mp4' }} style={homeScreenStyles.video} />
           <Card.Content>
             <Title>Temel Kaynak Teknikleri</Title>
             <Paragraph>Ahmet Yılmaz - Bu video, kaynak tekniklerinin temellerini öğrenmenize yardımcı olacaktır. Kaynak yapmanın incelikleri ve dikkat edilmesi gereken noktalar üzerinde durulmuştur.</Paragraph>
           </Card.Content>
         </Card>
         <Card style={homeScreenStyles.card}>
-          <CustomVideoPlayer source={require('../assets/videos/sample_video2.mp4')} style={homeScreenStyles.video} />
+          <CustomVideoPlayer source={{ uri: 'https://yenastorage.blob.core.windows.net/steelify/sample_video2.mp4' }} style={homeScreenStyles.video} />
           <Card.Content>
             <Title>İleri Seviye Büküm</Title>
             <Paragraph>Mehmet Kaya - Bu eğitim videosunda, ileri seviye büküm teknikleri ve bu tekniklerin farklı malzemeler üzerindeki uygulamaları detaylı bir şekilde anlatılmaktadır.</Paragraph>
           </Card.Content>
         </Card>
         <Card style={homeScreenStyles.card}>
-          <CustomVideoPlayer source={require('../assets/videos/sample_video3.mp4')} style={homeScreenStyles.video} />
+          <CustomVideoPlayer source={{ uri: 'https://yenastorage.blob.core.windows.net/steelify/sample_video3.mp4' }} style={homeScreenStyles.video} />
           <Card.Content>
             <Title>Özel Kaynak Uygulamaları</Title>
             <Paragraph>Ayşe Demir - Özel kaynak uygulamaları ve tekniklerinin yanı sıra, endüstride karşılaşılabilecek zorluklar ve bu zorlukların üstesinden nasıl gelinebileceği hakkında bilgiler verilmektedir.</Paragraph>
@@ -56,6 +56,7 @@ const HomeScreen = ({ navigation }) => {
         </Card.Content>
       </Card>
       <Card style={homeScreenStyles.blogCard} onPress={() => openLink('https://example.com/blog1')}>
+        <Card.Cover source={{ uri: 'https://yenastorage.blob.core.windows.net/steelify/steelify_sample_image2.jpg' }} />
         <Card.Content>
           <Title>Kaynak Tekniklerinde Uzmanlaşmak</Title>
           <Paragraph>Bu yazıda, kaynak tekniklerinde uzmanlaşmanın yollarını keşfedeceksiniz. Farklı kaynak tekniklerinin incelikleri ve pratik ipuçları ile uzman seviyesine ulaşmak için neler yapmanız gerektiğini öğrenin.</Paragraph>
@@ -65,6 +66,7 @@ const HomeScreen = ({ navigation }) => {
         </Card.Actions>
       </Card>
       <Card style={homeScreenStyles.blogCard} onPress={() => openLink('https://example.com/blog2')}>
+        <Card.Cover source={{ uri: 'https://yenastorage.blob.core.windows.net/steelify/steelify_sample_image3.jpg' }} />
         <Card.Content>
           <Title>Büküm Tekniklerinde Yenilikler</Title>
           <Paragraph>Büküm tekniklerinde son yenilikler ve uygulama alanları hakkında bilgi edinin. Bu makalede, endüstrideki en yeni büküm teknikleri ve bu tekniklerin avantajları ele alınmaktadır.</Paragraph>

38b561654f8abacb2a663f7051dc4aa897b57578
bbalbayrak <b.b.albayrak@hotmail.com>
2024-07-31 15:08:40 +0300

    Feature: Auth Module added.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(8082);
 }
 bootstrap();

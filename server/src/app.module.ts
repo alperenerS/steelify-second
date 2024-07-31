b0a7c60efad0ff8893e8f7330a87ec13ef3650da
bbalbayrak <b.b.albayrak@hotmail.com>
2024-07-31 15:20:05 +0300

    Feature: Database added.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './modules/core/database/database.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/modules/core/database/database.provider.ts
@@ -0,0 +1,31 @@
+import { Sequelize } from 'sequelize-typescript';
+import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
+import { databaseConfig } from './database.config';
+
+export const databaseProviders = [
+  {
+    provide: SEQUELIZE,
+    useFactory: async () => {
+      let config;
+      switch (process.env.NODE_ENV as any) {
+        case DEVELOPMENT:
+          config = databaseConfig.development;
+          break;
+        case TEST:
+          config = databaseConfig.test;
+          break;
+        case PRODUCTION:
+          config = databaseConfig.production;
+          break;
+        default:
+          config = databaseConfig.development;
+      }
+      const sequelize = new Sequelize(config);
+
+      sequelize.addModels([]);
+
+      await sequelize.sync();
+      return sequelize;
+    },
+  },
+];

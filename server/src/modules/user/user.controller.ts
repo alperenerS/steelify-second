7ff7fc3212787965cc881647bbe5e937be0c6a67
bbalbayrak <b.b.albayrak@hotmail.com>
2024-08-01 10:58:02 +0300

    Feature: Update User Info and Delete User Done.

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,15 +1,20 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   HttpStatus,
   NotFoundException,
   Param,
+  Put,
+  Req,
   Res,
   UseGuards,
 } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard/jwt.guard';
 import { UserService } from './user.service';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { UserDto } from './dto/user.dto';
 
 @Controller('api/user')
 @UseGuards(JwtGuard)
@@ -27,5 +32,27 @@ export class UserController {
     return res
       .status(HttpStatus.OK)
       .json({ message: 'User Info Fetched Successfully', data: user });
+  }
+
+  @Put('updateData')
+  async updateOwnData(
+    @Body() user: UserDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const token = req.headers.authorization.split(' ')[1];
+    const response = await this.userService.updateOwnData(user, token);
+
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'Successfully created !', data: response });
+  }
+
+  @Delete('delete/:id')
+  async deleteUser(@Param('id') id: number, @Res() res: Response) {
+    const data = await this.userService.deleteUser(id);
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'User Successfully Deleted !', data: data });
   }
 }
--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,13 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { USER_REPOSITORY } from '../../core/constants';
 import { User } from './user.entity';
 import { UserDto } from './dto/user.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
   constructor(
     @Inject(USER_REPOSITORY) private readonly userRepository: typeof User,
+    private readonly jwtService: JwtService,
   ) {}
 
   async findOneById(id: number): Promise<User> {
@@ -45,5 +47,42 @@ export class UserService {
     return await this.userRepository.findByPk(user_id, {
       attributes: { exclude: ['password', 'createdAt', 'updatedAt', 'id'] },
     });
+  }
+
+  async updateOwnData(user: UserDto, token: string) {
+    const decodedToken = this.jwtService.verify(token, {
+      publicKey: process.env.JWTKEY,
+    });
+    const userId = decodedToken.id;
+    const hashedPassword = await bcrypt.hash(user.password, 12);
+
+    const existingUser = this.userRepository.findByPk(userId);
+
+    if (!existingUser) {
+      throw new NotFoundException('User can not be found !');
+    }
+
+    const newUserDto: UserDto = {
+      email: user.email,
+      password: hashedPassword,
+      name: user.name,
+      surname: user.surname,
+      phoneNumber: user.phoneNumber,
+    };
+
+    return await this.userRepository.update(
+      {
+        email: newUserDto.email,
+        password: newUserDto.password,
+        name: newUserDto.name,
+        surname: newUserDto.surname,
+        phoneNumber: newUserDto.phoneNumber,
+      },
+      { where: { id: userId } },
+    );
+  }
+
+  async deleteUser(id: number) {
+    return await this.userRepository.destroy({ where: { id: id } });
   }
 }

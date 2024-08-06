442e8584ae64d7418e04a8516fe4363c287da555
bbalbayrak <b.b.albayrak@hotmail.com>
2024-08-06 17:12:02 +0300

    Feature: PIN IsValidate Done.

--- server/src/modules/pin/pin.controller.ts
+++ server/src/modules/pin/pin.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { PinService } from './pin.service';
+import { Request, Response } from 'express';
 
-@Controller('pin')
-export class PinController {}
+@Controller('api/pin')
+export class PinController {
+  constructor(private readonly pinService: PinService) {}
+
+  @Post('validate')
+  async validatePin(@Req() req: Request, @Res() res: Response) {
+    const { mail, pin } = req.body;
+
+    const isValidate = await this.pinService.validatePin(mail, pin);
+
+    return res.status(HttpStatus.OK).json({ data: isValidate });
+  }
+}

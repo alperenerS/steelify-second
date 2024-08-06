027316fa415d7e7831b3e9b93e5ca13fd3dcbd7a
emremataraci <mtrcemre@gmail.com>
2024-08-06 16:17:06 +0300

    Feature: Assesment Service p1

--- client/src/components/Assesment.js
+++ client/src/components/Assesment.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image } from 'react-native';
 import assesmentStyles from '../styles/AssesmentStyles';
 
 const Assesment = ({ uri }) => {

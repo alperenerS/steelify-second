98efb148b74f58e22e6cdcac118624fe814f4dd8
emremataraci <mtrcemre@gmail.com>
2024-08-06 14:12:17 +0300

    Feature: "Değerlendir" tab added to navigator + "Kamera" name changed to "Fotoğraf Çek"

--- client/src/navigation/TabNavigator.js
+++ client/src/navigation/TabNavigator.js
@@ -8,8 +8,8 @@ import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
 import ResetPasswordScreen from '../screens/ResetPasswordScreen';
 import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
+import CardScreen from '../screens/CardScreen';
 import { AuthContext } from '../context/AuthContext';
-
 
 const Tab = createBottomTabNavigator();
 
@@ -27,7 +27,7 @@ const TabNavigator = () => {
           let iconName;
           if (route.name === 'Anasayfa') {
             iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Kamera') {
+          } else if (route.name === 'Fotoğraf Çek') {
             iconName = focused ? 'camera' : 'camera-outline';
           } else if (route.name === 'Profil') {
             iconName = focused ? 'person' : 'person-outline';
@@ -39,6 +39,8 @@ const TabNavigator = () => {
             iconName = focused ? 'key' : 'key-outline';
           } else if (route.name === 'Şifremi Unuttum') {
             iconName = focused ? 'help' : 'help-outline';
+          } else if (route.name === 'Değerlendir') {
+            iconName = focused ? 'albums' : 'albums-outline';
           }
           return <Icon name={iconName} size={size} color={color} />;
         },
@@ -47,13 +49,14 @@ const TabNavigator = () => {
       })}
     >
       <Tab.Screen name="Anasayfa" component={HomeScreen} />
-      <Tab.Screen name="Kamera" component={CameraScreen} />
+      <Tab.Screen name="Fotoğraf Çek" component={CameraScreen} />
       {loggedIn ? (
-        <Tab.Screen name="Profil" component={ProfileScreen} />
-      ) : (
         <>
-          <Tab.Screen name="Giriş Yap" component={LoginScreen} />
+          <Tab.Screen name="Değerlendir" component={CardScreen} />
+          <Tab.Screen name="Profil" component={ProfileScreen} />
         </>
+      ) : (
+        <Tab.Screen name="Giriş Yap" component={LoginScreen} />
       )}
     </Tab.Navigator>
   );

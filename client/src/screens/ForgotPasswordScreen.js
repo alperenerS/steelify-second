a03fece1f145f94227062c876ba1092cc82442f1
emremataraci <mtrcemre@gmail.com>
2024-08-07 17:13:58 +0300

    Feature: ForgotPassword navigate/service/mail/style

--- client/src/screens/ForgotPasswordScreen.js
+++ client/src/screens/ForgotPasswordScreen.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import ForgotPasswordForm from '../components/ForgotPasswordForm';
 import forgotPasswordStyles from '../styles/ForgotPasswordStyles';
+import { sendResetPasswordEmail } from '../services/ForgotPasswordService';
 
 const ForgotPasswordScreen = () => {
-  const handleForgotPassword = (email) => {
-    // TODO: connect with API
-    console.log('Şifre sıfırlama için e-posta:', email);
-    // TODO: Mail send
+  const [error, setError] = useState('');
+  const navigation = useNavigation();
+
+  const handleForgotPassword = async (email) => {
+    try {
+      await sendResetPasswordEmail(email);
+      navigation.navigate('PIN');
+    } catch (error) {
+      setError('Şifre sıfırlama e-postası gönderilirken bir hata oluştu. Lütfen tekrar deneyin.');
+    }
   };
 
   return (
     <View style={forgotPasswordStyles.container}>
       <ForgotPasswordForm onSubmit={handleForgotPassword} />
+      {error && <Text style={forgotPasswordStyles.errorText}>{error}</Text>}
     </View>
   );
 };

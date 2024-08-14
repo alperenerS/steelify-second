5932f6499b35b4c855c317ea197ef1c9b838f9df
emremataraci <mtrcemre@gmail.com>
2024-08-15 00:25:51 +0300

    Feature: Login alert to custom popup

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import { View, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
 import Login from '../components/Login';
 import globalStyles from '../styles/GlobalStyles';
 import { loginUser } from '../services/LoginService';
+import CustomPopup from '../components/CustomPopup';
 
 const LoginScreen = ({ navigation }) => {
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [popupMessage, setPopupMessage] = useState('');
+  const [autoClose, setAutoClose] = useState(false);
+
   const handleLogin = async (email, password) => {
     try {
       const response = await loginUser(email, password);
       console.log('User logged in successfully:', response);
-      Alert.alert('Giriş Başarılı', 'Giriş işlemi başarıyla tamamlandı.', [
-        { text: 'OK', onPress: () => navigation.reset({
-            index: 0,
-            routes: [{ name: 'Tabs' }],
-          })
-        },
-      ]);
+      setPopupMessage('Giriş işlemi başarıyla tamamlandı.');
+      setPopupVisible(true);
+      setAutoClose(true);  // Popup'ın otomatik kapanması için ayarlandı
     } catch (error) {
       console.error('Login error:', error);
-      Alert.alert('Giriş Hatası', 'Giriş işlemi sırasında bir hata oluştu. Lütfen tekrar deneyin.');
+      setPopupMessage('Giriş işlemi sırasında bir hata oluştu. Lütfen tekrar deneyin.');
+      setPopupVisible(true);
+      setAutoClose(false);  // Hata durumunda otomatik kapanma olmayacak
     }
+  };
+
+  useEffect(() => {
+    if (popupVisible && autoClose) {
+      const timer = setTimeout(() => {
+        handleConfirm();
+      }, 3000);  // 3 saniye sonra otomatik kapatma
+
+      return () => clearTimeout(timer);  // Cleanup function
+    }
+  }, [popupVisible, autoClose]);
+
+  const handleConfirm = () => {
+    setPopupVisible(false);
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Tabs' }],
+    });
+  };
+
+  const handleClose = () => {
+    setPopupVisible(false);
   };
 
   const handleForgotPassword = () => {
@@ -32,7 +57,17 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={globalStyles.container}>
-      <Login onLogin={handleLogin} onForgotPassword={handleForgotPassword} onRegister={handleRegister} />
+      <Login
+        onLogin={handleLogin}
+        onForgotPassword={handleForgotPassword}
+        onRegister={handleRegister}
+      />
+      <CustomPopup
+        visible={popupVisible}
+        message={popupMessage}
+        onConfirm={handleConfirm}
+        onClose={handleClose}
+      />
     </View>
   );
 };

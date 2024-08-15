325ede35ba81df8e6f382f7cfb19fff1d164a796
emremataraci <mtrcemre@gmail.com>
2024-08-15 23:06:51 +0300

    Style: Camera Updates

--- client/src/styles/CameraStyles.js
+++ client/src/styles/CameraStyles.js
@@ -1,4 +1,6 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
 
 const CameraStyles = StyleSheet.create({
   container: {
@@ -6,40 +8,43 @@ const CameraStyles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
+    backgroundColor: '#FFFFFF',
   },
   preview: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'contain',
+    width: width * 0.9,
+    height: width * 0.9,
+    resizeMode: 'cover',
+    borderRadius: 15,
+    marginBottom: 20,
   },
   instructionsContainer: {
+    padding: 16,
+    borderRadius: 10,
     marginBottom: 20,
+    alignItems: 'flex-start',
   },
   instructionsTitle: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
-    textAlign: 'center',
-    color: '#000',
+    color: '#FF6F00',
   },
   instructionsText: {
     fontSize: 16,
     marginBottom: 8,
-    textAlign: 'left',
-    color: '#000',
+    color: '#333',
   },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
     width: '100%',
-    zIndex: 1,
     marginBottom: 20,
   },
   headerTitle: {
     fontSize: 22,
     fontWeight: 'bold',
-    color: '#000',
     marginLeft: 8,
+    color:"#000",
   },
 });
 
